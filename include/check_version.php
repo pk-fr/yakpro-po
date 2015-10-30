15abb546b631466e12b000ad26aa3dbbd9430f51
Pascal KISSIAN <github@lool.fr>
2015-10-30 14:58:44 +0100

    Update check_version.php

--- include/check_version.php
+++ include/check_version.php
@@ -11,15 +11,16 @@
 //          Use and abuse at your own risks.
 //========================================================================
 
+$yakpro_po_base_directory   = dirname(realpath($argv[0]));
 $php_parser_git_commandline = 'git clone --branch=1.x https://github.com/nikic/PHP-Parser.git';
 
-if(!file_exists('PHP-Parser/composer.json'))
+if(!file_exists("$yakpro_po_base_directory/PHP-Parser/composer.json"))
 {
     fprintf(STDERR,"Error:\tPHP-Parser is not correctly installed!%sYou can try to use the following command:%s\t# %s%s",PHP_EOL,PHP_EOL,$php_parser_git_commandline,PHP_EOL);
     exit(-1);
 }
 
-$t_composer             = json_decode(file_get_contents('PHP-Parser/composer.json'));   //print_r($t_composer);
+$t_composer             = json_decode(file_get_contents("$yakpro_po_base_directory/PHP-Parser/composer.json"));   //print_r($t_composer);
 $php_parser_branch      = $t_composer->{'extra'}->{'branch-alias'}->{'dev-master'} + 0;
 $required_php_version   = $t_composer->{'require'}->{'php'};
 
