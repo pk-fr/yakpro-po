3143ff4d670af677c08dbf304dffb9c33b9ac224
Pascal KISSIAN <github@lool.fr>
2015-12-12 19:14:33 +0100

    Create version.php

---
+++ version.php
@@ -0,0 +1,16 @@
+<?php
+//========================================================================
+// Author:  Pascal KISSIAN
+// Resume:  http://pascal.kissian.net
+//
+// Copyright (c) 2015 Pascal KISSIAN
+//
+// Published under the MIT License
+//          Consider it as a proof of concept!
+//          No warranty of any kind.
+//          Use and abuse at your own risks.
+//========================================================================
+
+$yakpro_po_version = "1.8.0";
+
+?>
