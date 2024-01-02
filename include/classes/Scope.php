14e3148d4c0405d8f93e1500363fc4951eaed215
Matěj Kmínek <m@kminet.eu>
2024-01-02 20:53:56 +0100

    feat: Add Scopes enum to easily specify ignored scopes

---
+++ include/classes/Scope.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Obfuscator\Classes;
+
+class Scope
+{
+    public const FUNCTION = "function";
+    public const CLSS = "class";
+    public const INTERFACE = "interface";
+    public const TRAIT = "trait";
+    public const NAMESPACE = "namespace";
+    public const VARIABLE = "variable";
+    public const METHOD = "method";
+    public const PROPERTY = "property";
+    public const CLASS_CONSTANT = "class_constant";
+    public const CONSTANT = "constant";
+    public const LABEL = "label";
+}
